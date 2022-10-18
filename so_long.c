b8e261cd8d32a52e9abcaa96ef107a58e83c61b6
Gilles Poncelet <gponcele@mi-r5-p1.s19.be>
2022-10-18 17:50:35 +0200

    MAJ

--- so_long.c
+++ so_long.c
@@ -6,21 +6,42 @@
 /*   By: gponcele <gponcele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 16:30:03 by gponcele          #+#    #+#             */
-/*   Updated: 2022/10/18 17:07:31 by gponcele         ###   ########.fr       */
+/*   Updated: 2022/10/18 17:46:21 by gponcele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 #include "MLX/mlx.h"
 
+int	deal_key(int key, void *param)
+{
+	(void)param;
+	ft_putnbr_fd(key, 1);
+	return (0);
+}
+
 int	main(int argc, char **argv)
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
+	int		x;
+	int		y;
 
 	(void)argc;
 	(void)argv;
+	x = 0;
 	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "test");
+	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "So long");
+	while (x <= 500)
+	{
+		y = 0;
+		while (y <= 500)
+		{
+			mlx_pixel_put(mlx_ptr, win_ptr, x, y, 0x002BFF);
+			y++;
+		}
+		x++;
+	}
+	mlx_key_hook(win_ptr, deal_key, (void *)0);
 	mlx_loop(mlx_ptr);
 }
